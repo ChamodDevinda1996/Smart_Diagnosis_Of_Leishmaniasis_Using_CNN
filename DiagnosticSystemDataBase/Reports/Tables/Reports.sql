e478444edf0984913d52472495496a2394d593c0
cdevinda <chamoddevinda0000@gmail.com>
2022-03-07 22:30:20 +0530

    CD_  Db tables added

---
+++ DiagnosticSystemDataBase/Reports/Tables/Reports.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Reports].[Reports] (
+    [ReportId]    BIGINT          IDENTITY (1, 1) NOT NULL,
+    [UserId]      VARCHAR (128)   NOT NULL,
+    [CreatedDate] DATETIME2 (7)   NOT NULL,
+    [Result]      VARBINARY (MAX) NOT NULL,
+    CONSTRAINT [PK_Reports] PRIMARY KEY CLUSTERED ([ReportId] ASC)
+);
+
